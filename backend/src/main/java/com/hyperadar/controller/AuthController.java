e4615d5a240ecc8c145e118ead3f993d04118f80
sbhat72 <sbhat72@uwo.ca>
2026-06-08 22:08:54 -0400

    Added Register endpoint for user to sign up

--- backend/src/main/java/com/hyperadar/controller/AuthController.java
+++ backend/src/main/java/com/hyperadar/controller/AuthController.java
@@ -3,8 +3,26 @@ package com.hyperadar.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hyperadar.dto.AuthRequestDto;
+import com.hyperadar.dto.AuthResponseDto;
+import com.hyperadar.service.UserService;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
-    // TODO: implement authentication endpoints
+    private final UserService userService;
+    
+    
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDto> register(@RequestBody AuthRequestDto authRequest) {
+        return ResponseEntity.ok(userService.registerUser(authRequest));
+    }
 }
